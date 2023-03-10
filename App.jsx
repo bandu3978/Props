d2b3ab234c91faa5ed27eafca5ac28352ac74ef7
bandu3978 <120469400+bandu3978@users.noreply.github.com>
2023-03-10 12:03:17 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import StudentDetails from "./JSON in PROPS/StudentDetails.jsx";
+import json from "./JSON in PROPS/student.json"
+import "./JSON in PROPS/globle.css"
+
+
+
+const App=()=>
+{
+  return(
+  
+<>
+
+   <StudentDetails data1={json} 
+
+
+
+/> 
+   </>
+ 
+  )
+}
+export default App
---
+++ StudentDetails.jsx
@@ -0,0 +1,175 @@
+import React from "react";
+
+const StudentDetails=(props)=>
+{
+  return(
+    <>
+ <div className="main">
+      <div className="row">
+        <div className="box">
+    <img src={props.data1[0].studentimage} alt="text" />
+    <h2>{props.data1[0].StudentName}</h2>
+    <h2>{props.data1[0].StudentEmail}</h2>
+    <h2>{props.data1[0].Mobilenumber}</h2>
+    <h2>{props.data1[0].place}</h2>
+    </div>
+    <div className="box">
+    <img src={props.data1[1].studentimage} alt="text" />
+    <h2>{props.data1[1].StudentName}</h2>
+    <h2>{props.data1[1].StudentEmail}</h2>
+    <h2>{props.data1[1].Mobilenumber}</h2>
+    <h2>{props.data1[1].place}</h2>
+    </div>
+    <div className="box">
+    <img src={props.data1[2].studentimage} alt="text" />
+    <h2>{props.data1[2].StudentName}</h2>
+    <h2>{props.data1[2].StudentEmail}</h2>
+    <h2>{props.data1[2].Mobilenumber}</h2>
+    <h2>{props.data1[2].place}</h2>
+    </div>
+    <div className="box">
+    <img src={props.data1[3].studentimage} alt="text" />
+    <h2>{props.data1[3].StudentName}</h2>
+    <h2>{props.data1[3].StudentEmail}</h2>
+    <h2>{props.data1[3].Mobilenumber}</h2>
+    <h2>{props.data1[3].place}</h2>
+    </div>
+    <div className="box">
+    <img src={props.data1[4].studentimage} alt="text" />
+        <h2>{props.data1[4].StudentName}</h2>
+    <h2>{props.data1[4].StudentEmail}</h2>
+    <h2>{props.data1[4].Mobilenumber}</h2>
+    <h2>{props.data1[4].place}</h2>
+    </div>
+    
+     </div>
+
+     <div className="row">
+        <div className="box">
+        <img src={props.data1[5].studentimage} alt="text" />
+    <h2>{props.data1[5].StudentName}</h2>
+    <h2>{props.data1[5].StudentEmail}</h2>
+    <h2>{props.data1[5].Mobilenumber}</h2>
+    <h2>{props.data1[5].place}</h2>
+    </div>
+    <div className="box">
+    <img src={props.data1[6].studentimage} alt="text" />
+    <h2>{props.data1[6].StudentName}</h2>
+    <h2>{props.data1[6].StudentEmail}</h2>
+    <h2>{props.data1[6].Mobilenumber}</h2>
+    <h2>{props.data1[6].place}</h2>
+    </div>
+    <div className="box">
+    <img src={props.data1[7].studentimage} alt="text" />
+    <h2>{props.data1[7].StudentName}</h2>
+    <h2>{props.data1[7].StudentEmail}</h2>
+    <h2>{props.data1[7].Mobilenumber}</h2>
+    <h2>{props.data1[7].place}</h2>
+    </div>
+    <div className="box">
+    <img src={props.data1[8].studentimage} alt="text" />
+    <h2>{props.data1[8].StudentName}</h2>
+    <h2>{props.data1[8].StudentEmail}</h2>
+    <h2>{props.data1[8].Mobilenumber}</h2>
+    <h2>{props.data1[8].place}</h2>
+    </div>
+    <div className="box">
+    <img src={props.data1[9].studentimage} alt="text" />
+    <h2>{props.data1[9].StudentName}</h2>
+    <h2>{props.data1[9].StudentEmail}</h2>
+    <h2>{props.data1[9].Mobilenumber}</h2>
+    <h2>{props.data1[9].place}</h2>
+    </div>
+    
+     </div>
+
+     <div className="row">
+        <div className="box">
+        <img src={props.data1[10].studentimage} alt="text" />
+    <h2>{props.data1[10].StudentName}</h2>
+    <h2>{props.data1[10].StudentEmail}</h2>
+    <h2>{props.data1[10].Mobilenumber}</h2>
+    <h2>{props.data1[10].place}</h2>
+    </div>
+    <div className="box">
+    <img src={props.data1[11].studentimage} alt="text" />
+    <h2>{props.data1[11].StudentName}</h2>
+    <h2>{props.data1[11].StudentEmail}</h2>
+    <h2>{props.data1[11].Mobilenumber}</h2>
+    <h2>{props.data1[11].place}</h2>
+    </div>
+    <div className="box">
+    <img src={props.data1[12].studentimage} alt="text" />
+    <h2>{props.data1[12].StudentName}</h2>
+    <h2>{props.data1[12].StudentEmail}</h2>
+    <h2>{props.data1[12].Mobilenumber}</h2>
+    <h2>{props.data1[12].place}</h2>
+    </div>
+    <div className="box">
+    <img src={props.data1[13].studentimage} alt="text" />
+    <h2>{props.data1[13].StudentName}</h2>
+    <h2>{props.data1[13].StudentEmail}</h2>
+    <h2>{props.data1[13].Mobilenumber}</h2>
+    <h2>{props.data1[13].place}</h2>
+    </div>
+    <div className="box">
+    <img src={props.data1[14].studentimage} alt="text" />
+    <h2>{props.data1[14].StudentName}</h2>
+    <h2>{props.data1[14].StudentEmail}</h2>
+    <h2>{props.data1[14].Mobilenumber}</h2>
+    <h2>{props.data1[14].place}</h2>
+    </div>
+    
+     </div>
+
+
+     <div className="row">
+        <div className="box">
+        <img src={props.data1[15].studentimage} alt="text" />
+    <h2>{props.data1[15].StudentName}</h2>
+    <h2>{props.data1[15].StudentEmail}</h2>
+    <h2>{props.data1[15].Mobilenumber}</h2>
+    <h2>{props.data1[15].place}</h2>
+    </div>
+    <div className="box">
+    <img src={props.data1[16].studentimage} alt="text" />
+    <h2>{props.data1[16].StudentName}</h2>
+    <h2>{props.data1[16].StudentEmail}</h2>
+    <h2>{props.data1[16].Mobilenumber}</h2>
+    <h2>{props.data1[16].place}</h2>
+    </div>
+    <div className="box">
+    <img src={props.data1[17].studentimage} alt="text" />
+    <h2>{props.data1[17].StudentName}</h2>
+    <h2>{props.data1[17].StudentEmail}</h2>
+    <h2>{props.data1[17].Mobilenumber}</h2>
+    <h2>{props.data1[17].place}</h2>
+    </div>
+    <div className="box">
+    <img src={props.data1[18].studentimage} alt="text" />
+    <h2>{props.data1[18].StudentName}</h2>
+    <h2>{props.data1[18].StudentEmail}</h2>
+    <h2>{props.data1[18].Mobilenumber}</h2>
+    <h2>{props.data1[18].place}</h2>
+    </div>
+    <div className="box">
+    <img src={props.data1[19].studentimage} alt="text" />
+    <h2>{props.data1[19].StudentName}</h2>
+    <h2>{props.data1[19].StudentEmail}</h2>
+    <h2>{props.data1[19].Mobilenumber}</h2>
+    <h2>{props.data1[19].place}</h2>
+    </div>
+    
+     </div>
+
+
+
+
+
+ </div>
+    </>
+   
+    
+  )
+}
+export default StudentDetails
